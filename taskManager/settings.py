10160234a5a910a882ef8214d966da3a50828b00
Syakir Ali <email02081997@gmail.com>
2021-04-15 20:21:54 +0700

    update allowed host

--- taskManager/settings.py
+++ taskManager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = '0yxzudryd8)-%)(fz&7q-!v&cq1u6vbfoc4u7@u_&i)b@4eh^q'
 DEBUG = False
 TEMPLATE_DEBUG = False
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
